01e3ad756eff52fb34ace3e74bacbd29b6d081e6
Shuvo <shuvombm02@gmail.com>
2020-04-17 00:15:06 +0600

    Json data added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
